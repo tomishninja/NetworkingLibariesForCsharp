d9b58b2f048b963765563f9958559549c8216754
Thomas Clarke <tomishninja@gmail.com>
2019-02-14 14:34:17 +1030

    Message Helper has been documented

--- NetworkingLibaryStandard/MessageHelper.cs
+++ NetworkingLibaryStandard/MessageHelper.cs
@@ -4,8 +4,16 @@ using System.Text;
 
 namespace NetworkingLibaryStandard
 {
+    /// <summary>
+    /// This class contains various elements to help this libary 
+    /// communicate to other parts of the system as it runs.
+    /// </summary>
     public class MessageHelper
     {
+        /// <summary>
+        /// A value decribing different types of messages sent from this libary
+        /// that could be used for general purpose.
+        /// </summary>
         public enum MessageType { Data, Exception, Error };
     }
 }
