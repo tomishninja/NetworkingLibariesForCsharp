abcc7e4003372c21cbd4ca605fc007a9c9f0d813
Thomas Clarke <tomishninja@gmail.com>
2019-02-19 11:26:09 +1030

    added comments to IResponder

--- NetworkingLibrary/IResponder.cs
+++ NetworkingLibrary/IResponder.cs
@@ -6,8 +6,20 @@ using System.Threading.Tasks;
 
 namespace NetworkingLibrary
 {
+    /// <summary>
+    /// This interface allows classes in the Networking libary to
+    /// send various interactions. More still needs to be done with this class
+    /// before its intergartion
+    /// </summary>
     interface IResponder
     {
+        /// <summary>
+        /// This method will be called to determine various 
+        /// responces that can be called vai the network server.
+        /// </summary>
+        /// <param name="MessageRecived">
+        /// The data recived from the message recived.
+        /// </param>
         void Respond(string MessageRecived);
     }
 }
