1492ce512d044447dfdd80c0114d6577827b0414
Thomas Clarke <tomishninja@gmail.com>
2019-02-19 11:29:59 +1030

    wrote documention for IDisplayMessage

--- NetworkingLibrary/iDisplayMessage.cs
+++ NetworkingLibrary/iDisplayMessage.cs
@@ -6,8 +6,19 @@ using System.Threading.Tasks;
 
 namespace NetworkingLibrary
 {
+    /// <summary>
+    /// Allows comunication from the network libary to a class
+    /// that this is connected to.
+    /// </summary>
     public interface IDisplayMessage
     {
+        /// <summary>
+        /// sends a message to a object from the network class
+        /// so long as this object is used as a parameter in the constuctor.
+        /// </summary>
+        /// <param name="message">
+        /// The message text from the network libary
+        /// </param>
         void DisplayMessage(string message);
     }
 }
